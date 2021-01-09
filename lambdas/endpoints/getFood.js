68c5d178c9d17114bcceb46b48e59f5f72ba1436
James Thomas <codeactor@protonmail.com>
2021-01-09 15:53:21 +0000

    Revert "Set as const"
    This reverts commit f346c9f7a9dbcf51bf99c201ea52b0a3532605be.

--- lambdas/endpoints/getFood.js
+++ lambdas/endpoints/getFood.js
@@ -8,7 +8,7 @@ exports.handler = async (event) => {
     return Responses._400({ message: 'Missing the food ID from the path' });
   }
 
-  const ID = event.pathParameters.ID;
+  let ID = event.pathParameters.ID;
 
   const food = await Dynamo.get(ID, tableName).catch(() => {
     return null;
