f94d91286c329fa370c38ff1398f9394734b3a10
James Thomas <codeactor@protonmail.com>
2021-01-09 16:02:28 +0000

    Reject submissions without a count

--- lambdas/endpoints/addFood.js
+++ lambdas/endpoints/addFood.js
@@ -6,6 +6,10 @@ const tableName = process.env.mainTableName;
 exports.handler = async (event) => {
   const foodData = JSON.parse(event.body);
 
+  if (!foodData.count) {
+    return Responses._400({ message: 'No count provided.' });
+  }
+
   const newFood = await Dynamo.write(foodData, tableName).catch(() => {
     return null;
   });
