938779c3fd60e0b9a5f63f4c4f3beb470216cfbe
James Thomas <codeactor@protonmail.com>
2021-01-10 15:38:28 +0000

    Recipe retrieveal endpoint

---
+++ lambdas/endpoints/getRecipes.js
@@ -0,0 +1,27 @@
+const Responses = require('../common/responses');
+const Dynamo = require('../common/dynamo');
+
+const tableName = process.env.recipeTableName;
+
+exports.handler = async (event) => {
+  const recipesData = JSON.parse(event.body);
+  const { recipes } = recipesData;
+
+  if (!recipes || !Array.isArray(recipes) || recipes.length === 0) {
+    return Responses.response400({ message: 'No recipes requested.' });
+  }
+
+  const recipeNames = recipes.map((recipe) => recipe.ID);
+
+  const recipesResponse = await Promise.all(
+    recipeNames.map(async (name) => {
+      return Dynamo.get(name, tableName).catch(() => {
+        return Responses.response400({
+          message: `Failed to retrieve recipes: ${recipesData}`,
+        });
+      });
+    }),
+  );
+
+  return Responses.response200({ recipesResponse });
+};
