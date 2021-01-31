8e978195b04c809dd25a86d6dfa38dc699a974d3
James Thomas <codeactor@protonmail.com>
2021-01-31 13:14:40 +0000

    Test for valid submission

--- lambdas/common/validate.js
+++ lambdas/common/validate.js
@@ -2,7 +2,10 @@ const Joi = require('joi');
 
 const ingredient = Joi.object({
   id: Joi.string().alphanum().min(3).required(),
-  name: Joi.string().alphanum().min(3).required(),
+  name: Joi.string()
+    .regex(/^[a-zA-Z0-9 ]*$/)
+    .min(3)
+    .required(),
   amount: Joi.number().min(1).required(),
 });
 
--- tests/endpoints/addIngredients.test.js
+++ tests/endpoints/addIngredients.test.js
@@ -1,5 +1,7 @@
+const Dynamo = require('../../lambdas/common/dynamo');
 const addIngredients = require('../../lambdas/endpoints/addIngredients');
 const ingredientsMissing = require('../data/ingredientsMissing.json');
+const ingredientsValid = require('../data/ingredientsValid.json');
 
 const convertToPayload = (data) => {
   const payload = JSON.stringify(data);
@@ -16,6 +18,15 @@ describe('addIngredients endpoint', () => {
       expect(responseBody.message).toBe(
         '"ingredients" must contain at least 1 items',
       );
+    });
+  });
+
+  describe('Valid ingredients submission', () => {
+    it('Should return a 200 response for a valid submission', async () => {
+      jest.spyOn(Dynamo, 'write').mockImplementation(() => Promise.resolve());
+      const payload = convertToPayload(ingredientsValid);
+      const response = await addIngredients.handler(payload);
+      expect(response.statusCode).toBe(200);
     });
   });
 });
