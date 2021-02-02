19ab8f4af0efdacb64c969af56319997b7a0c9b4
James Thomas <codeactor@protonmail.com>
2021-02-02 15:39:06 +0000

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const { node } = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: node,
+  mode: 'production',
+  externals: [nodeExternals()],
+};
