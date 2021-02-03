b9c225ed2f32917a16c4a3d03f6cc7e85514459a
James Thomas <codeactor@protonmail.com>
2021-02-03 10:40:38 +0000

    Add babel and complete webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
+const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
+  entry: slsw.lib.entries,
   target: 'node',
-  mode: 'production',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   externals: [nodeExternals()],
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+  },
 };
