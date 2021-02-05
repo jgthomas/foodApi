5c622d5beeb40fa2b479f3d7254725a0d52b9c3f
James Thomas <codeactor@protonmail.com>
2021-02-05 09:53:56 +0000

    Add chunking method

--- lambdas/common/helper.js
+++ lambdas/common/helper.js
@@ -6,6 +6,17 @@ const Helper = {
   errorInserting(response) {
     return response[0].failMessage !== undefined;
   },
+
+  chunk(array, size) {
+    const chunkedArray = [];
+    let index = 0;
+    while (index < array.length) {
+      chunkedArray.push(array.slice(index, size + index));
+      index += size;
+    }
+
+    return chunkedArray;
+  },
 };
 
 export default Helper;
